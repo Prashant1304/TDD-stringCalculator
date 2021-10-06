53fa2e30b0fe6ebac327bdccac66fee535d315f2
Prashant1304 <sinha.prashant134@gmail.com>
2021-10-07 04:15:52 +0530

    compleated first step

--- component/string.js
+++ component/string.js
@@ -1,10 +1,30 @@
 function Add(string) {
-    split = string.split("");
+    let output = 0
+
+    split = string.split(",");
+    
+    for(var i = 0;i < split.length; i++ ) {
+        let number = +split[i]
+        output+=number
+        
+    }    
     
     if(string == "") {
         return 0
+    } else  {
+        return output
     }
     
 }
+
+
+    let split= "1,2,3".split(",");
+    let output = 0
+    
+    for(var i = 0;i < split.length; i++ ) {
+        let number = +split[i]
+        output+=number
+    }
+    console.log(output)    
   
 module.exports = Add
--- test/string.test.js
+++ test/string.test.js
@@ -4,3 +4,10 @@ test("return 0 when string is empty", () => {
     expect(add("")).toBe(0);
 })
 
+test("return single no if single no is provided", () => {
+    expect(add("1")).toBe(1)
+})
+
+test("return expected sum of multi numbers", () => {
+    expect(add("1,2,3")).toBe(6)
+})
